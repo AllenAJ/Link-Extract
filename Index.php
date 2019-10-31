98ca9be1c33e72dc5911450faa0bbe3fb5085145
Allen Joseph <allenxavier45@gmail.com>
2019-10-31 20:01:08 +0530

    Improved the code by handling the URL .

--- Index.php
+++ Index.php
@@ -1,5 +1,5 @@
 <?php
-$start = ""; //Place your link here
+$start = "https://www.youtube.com/";
 
 function follow_links($url) {
     $doc = new DOMDocument();
@@ -8,7 +8,25 @@ function follow_links($url) {
     $linklist = $doc->getElementsByTagName("a");
 
     foreach($linklist as $link){
-        echo $link->getAttribute("href")."\n";
+        $l = $link->getAttribute("href");
+
+        if (substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
+		} else if (substr($l, 0, 2) == "//") {
+			$l = parse_url($url)["scheme"].":".$l;
+		} else if (substr($l, 0, 2) == "./") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l, 1);
+		} else if (substr($l, 0, 1) == "#") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;
+		} else if (substr($l, 0, 3) == "../") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+		} else if (substr($l, 0, 11) == "javascript:") {
+			continue;
+		} else if (substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+        }
+        
+        echo $l."\n";
     }
     
 }
