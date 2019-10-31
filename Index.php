08fe265dabfe278fc4052cf5a46eea613472550a
Allen Joseph <allenxavier45@gmail.com>
2019-10-31 19:27:18 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,16 @@
+<?php
+$start = ""; //Place your link here
+
+function follow_links($url) {
+    $doc = new DOMDocument();
+    $doc->loadHTML(file_get_contents($url));
+    
+    $linklist = $doc->getElementsByTagName("a");
+
+    foreach($linklist as $link){
+        echo $link->getAttribute("href")."\n";
+    }
+    
+}
+
+follow_links($start);
